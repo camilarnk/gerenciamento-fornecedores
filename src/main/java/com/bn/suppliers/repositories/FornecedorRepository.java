3d9fa9456ed727017c35804fe099c6c1baec621f
Camila Reinke <reinkecamila@gmail.com>
2026-03-21 14:44:21 -0300

    feat: cria repositório FornecedorRepository

---
+++ src/main/java/com/bn/suppliers/repositories/FornecedorRepository.java
@@ -0,0 +1,6 @@
+package com.bn.suppliers.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<FornecedorRepository, Long> {
+}
